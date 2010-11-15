7eae3c6cc2e0efa1d83f8a53c89e696374474308
Xinwei.Yi@gmail.com <Xinwei.Yi@gmail.com@021c3f65-3e9a-cb85-632d-0ac7946d1f4a>
2010-11-15 03:49:55 +0000

    release 01.00.03 version

--- PhoneSecurity/src/com/herry/phonesecurity/Prefs.java
+++ PhoneSecurity/src/com/herry/phonesecurity/Prefs.java
@@ -7,40 +7,52 @@ public class Prefs {
 	private static final String PREF_NAME = "pref";
 	private static final String OLD_SIM = "old_sim";
 	private static final String NEW_SIM = "new_sim";
+	private static final String SETTING_REMINDER = "setting_reminder";
 
 	public static SharedPreferences get(Context ctx) {
 		return ctx.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
 	}
-	
-	public static String getOldSim(Context ctx){
+
+	public static String getOldSim(Context ctx) {
 		SharedPreferences pref = get(ctx);
 		return pref.getString(OLD_SIM, null);
 	}
-	
-	public static boolean setOldSim(Context ctx,String sim){
+
+	public static boolean setOldSim(Context ctx, String sim) {
 		SharedPreferences pref = get(ctx);
 		SharedPreferences.Editor editor = pref.edit();
 		editor.putString(OLD_SIM, sim);
 		return editor.commit();
 	}
-	
-	public static String getNewSim(Context ctx){
+
+	public static String getNewSim(Context ctx) {
 		SharedPreferences pref = get(ctx);
 		return pref.getString(NEW_SIM, null);
 	}
-	
-	public static boolean setNewSim(Context ctx,String sim){
+
+	public static boolean setNewSim(Context ctx, String sim) {
 		SharedPreferences pref = get(ctx);
 		SharedPreferences.Editor editor = pref.edit();
 		editor.putString(NEW_SIM, sim);
 		return editor.commit();
 	}
-	
-	public static boolean reset(Context ctx){
+
+	public static boolean getReminder(Context ctx) {
+		SharedPreferences pref = get(ctx);
+		return pref.getBoolean(SETTING_REMINDER, true);
+	}
+
+	public static boolean setReminder(Context ctx) {
+		SharedPreferences pref = get(ctx);
+		SharedPreferences.Editor editor = pref.edit();
+		editor.putBoolean(SETTING_REMINDER, false);
+		return editor.commit();
+	}
+
+	public static boolean reset(Context ctx) {
 		SharedPreferences pref = get(ctx);
 		SharedPreferences.Editor editor = pref.edit();
 		editor.clear();
 		return editor.commit();
 	}
-	
 }
