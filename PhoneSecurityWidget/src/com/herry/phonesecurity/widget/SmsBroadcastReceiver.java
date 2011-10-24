973d06a6b23c5a0e2075532a718f1855ce120051
Xinwei.Yi@gmail.com <Xinwei.Yi@gmail.com@021c3f65-3e9a-cb85-632d-0ac7946d1f4a>
2011-10-24 15:21:32 +0000

    add project PhoneSecurityWidget

---
+++ PhoneSecurityWidget/src/com/herry/phonesecurity/widget/SmsBroadcastReceiver.java
@@ -0,0 +1,33 @@
+package com.herry.phonesecurity.widget;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+public class SmsBroadcastReceiver extends BroadcastReceiver {
+	private static final String TAG = "SmsBroadcastReceiver";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		String action = intent.getAction();
+		Log.d(TAG, "action : " + action);
+		if (action.equals(Const.ACTION_SMS_RECEIVED)) {
+			String smsBody = Utils.getSmsBody(intent);
+			String alarmPwd = Prefs.getAlarmPwd(context);
+			String selfNumber = Utils.getSelfNumber(context);
+			StringBuilder sb = new StringBuilder();
+			sb.append(selfNumber).append(alarmPwd).append(Const.ALARM);
+			if (alarmPwd != null && selfNumber != null
+					&& !"".equals(smsBody.trim())
+					&& smsBody.startsWith(sb.toString())) {
+				intent.setData(Uri.parse(Const.ALARM));
+				intent.setClass(context, HandleAlarmService.class);
+				context.startService(intent);
+				abortBroadcast();
+			}
+		}
+	}
+
+}
