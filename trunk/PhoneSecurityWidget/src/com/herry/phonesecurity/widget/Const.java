830ba46978f8a4463460c099a866d3cb272e6946
Xinwei.Yi@gmail.com <Xinwei.Yi@gmail.com@021c3f65-3e9a-cb85-632d-0ac7946d1f4a>
2011-10-24 15:21:32 +0000

    add project PhoneSecurityWidget

---
+++ trunk/PhoneSecurityWidget/src/com/herry/phonesecurity/widget/Const.java
@@ -0,0 +1,26 @@
+package com.herry.phonesecurity.widget;
+
+public class Const {
+	public static final boolean mbTest = false;
+	// self define
+	public static final String ACTION_SENT = "com.herry.phonesecurity.intent.SENT";
+	public static final String ACTION_DELIVERED = "com.herry.phonesecurity.intent.DELIVERED";
+
+	// sim change broadcast action
+	public static final String ACTION_SIM_CHANGED = "android.intent.action.SIM_STATE_CHANGED";
+
+	// sms received broadcast action
+	public static final String ACTION_SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+	
+	//sms content match const
+	public static final String ALARM = "alarm";//paly a ringtone
+	
+	
+	/**
+	 * style text const
+	 */
+	public static final String FONT_START = "<font color=\"#00ffff\"><i>";
+	public static final String FONT_END = "</i></font>";
+	public static final String NEW_LINE = "<br>";
+	public static final String REPLACED = "{?}";
+}
