ea33fa49adadb3691f14af26a14686ab74db1247
Xinwei.Yi@gmail.com <Xinwei.Yi@gmail.com@021c3f65-3e9a-cb85-632d-0ac7946d1f4a>
2011-10-24 15:15:31 +0000

    add project PhoneSecurityWidget

---
+++ trunk/src/com/herry/phonesecurity/widget/preferences/DialogPreference.java
@@ -0,0 +1,54 @@
+package com.herry.phonesecurity.widget.preferences;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.util.AttributeSet;
+
+import com.herry.phonesecurity.widget.Prefs;
+import com.herry.phonesecurity.widget.R;
+import com.herry.phonesecurity.widget.Utils;
+
+public class DialogPreference extends android.preference.DialogPreference {
+	private Context mCtx;
+
+	public DialogPreference(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		mCtx = context;
+	}
+
+	public DialogPreference(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		mCtx = context;
+	}
+
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		super.onClick(dialog, which);
+		switch (which) {
+		case DialogInterface.BUTTON_POSITIVE:
+			Prefs.reset(mCtx);
+			String imsi = Utils.getIMSI(mCtx);
+			if (imsi != null) {
+				Prefs.setOldSim(mCtx, imsi);
+			} else {
+				new AlertDialog.Builder(mCtx).setIcon(
+						android.R.drawable.ic_dialog_alert).setTitle(
+						R.string.no_sim_title).setMessage(R.string.no_sim_msg)
+						.setPositiveButton(android.R.string.ok,
+								new DialogInterface.OnClickListener() {
+
+									@Override
+									public void onClick(DialogInterface dialog,
+											int which) {
+										((Activity) mCtx).finish();
+
+									}
+
+								}).setCancelable(false).create().show();
+			}
+			break;
+		}
+	}
+}
