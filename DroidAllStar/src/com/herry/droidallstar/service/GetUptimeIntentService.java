baa69517edeafac2ae847ae82214714b39bc81ab
Xinwei.Yi@gmail.com <Xinwei.Yi@gmail.com@021c3f65-3e9a-cb85-632d-0ac7946d1f4a>
2011-10-25 10:00:53 +0000

    error handle

--- DroidAllStar/src/com/herry/droidallstar/service/GetUptimeIntentService.java
+++ DroidAllStar/src/com/herry/droidallstar/service/GetUptimeIntentService.java
@@ -32,6 +32,6 @@ public class GetUptimeIntentService extends IntentService {
 
 	public static void runIntentService(Context ctx, Intent intent) {
 		intent.setClass(ctx, GetUptimeIntentService.class);
-		ctx.startActivity(intent);
+		ctx.startService(intent);
 	}
 }
