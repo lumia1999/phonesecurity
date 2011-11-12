086449d75882530833229d756bbacbd8556c7644
Xinwei.Yi@gmail.com <Xinwei.Yi@gmail.com@021c3f65-3e9a-cb85-632d-0ac7946d1f4a>
2011-11-12 07:51:05 +0000

    1.add rom tip

--- FastAppMgr/src/com/herry/fastappmgr/util/Constants.java
+++ FastAppMgr/src/com/herry/fastappmgr/util/Constants.java
@@ -5,4 +5,5 @@ public final class Constants {
 	// youmi offer show time interval
 	public static final long YOUMIOFFER_MAX_TIMESTAMP = 2 * 24 * 60 * 60
 			* 1000L;
+	public static final String ACTION_UPDATE_ROM = "com.herry.fastappmgr.intent.ACTION_UPDATE_ROM";
 }
