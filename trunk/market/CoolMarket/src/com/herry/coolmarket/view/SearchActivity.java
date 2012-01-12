c909be10c85182c1260acc64bf3d8dc2b808aab2
Xinwei.Yi@gmail.com <Xinwei.Yi@gmail.com@021c3f65-3e9a-cb85-632d-0ac7946d1f4a>
2012-01-12 17:15:35 +0000

    主要修改：实现搜索功能中。。。

--- trunk/market/CoolMarket/src/com/herry/coolmarket/view/SearchActivity.java
+++ trunk/market/CoolMarket/src/com/herry/coolmarket/view/SearchActivity.java
@@ -11,6 +11,7 @@ import org.xmlpull.v1.XmlPullParserFactory;
 
 import com.herry.coolmarket.R;
 import com.herry.coolmarket.util.Constants;
+import com.herry.coolmarket.util.LoadingDrawable;
 
 import android.app.Activity;
 import android.content.res.Resources;
@@ -18,17 +19,47 @@ import android.content.res.Resources.NotFoundException;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.View.OnClickListener;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SearchActivity extends Activity {
 	private static final String TAG = "SearchActivity";
+
+	// search top
+	private EditText mKeysEdit;
+	private ImageButton mSearchBtn;
+
+	// suggest
 	Resources mRes;
 	private LinearLayout mSuggestLayout;
+	private LayoutAnimationController mSuggestLayoutAnimController;
 	private SuggestWidget mSuggestWidget;
 	private String mSuggestKeys;
 	private String[] mCurrentUseKeys;
+	private GestureDetector mGestureDetector;
+
+	// loading
+	private ProgressBar mProgressBar;
+	private LoadingDrawable mLoadingDrawable;
+
+	// search result
+	private ListView mListView;
+
+	// retry
+	private TextView mRetryTxt;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +67,47 @@ public class SearchActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.search);
 		initUI();
-		test();
+		getDefaultKeys();
 		fillSuggest();
 	}
 
 	private void initUI() {
+		mKeysEdit = (EditText) findViewById(R.id.fact_search_keysedit);
+		mSearchBtn = (ImageButton) findViewById(R.id.fact_search_btn);
+		mSearchBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				String key = mKeysEdit.getText().toString();
+				if (key == null || "".equals(key.trim())) {
+					Toast.makeText(getApplicationContext(),
+							R.string.invalid_key_word_toast, Toast.LENGTH_SHORT)
+							.show();
+				} else {
+					onSearch(key);
+				}
+			}
+		});
+		mProgressBar = (ProgressBar) findViewById(android.R.id.progress);
+		mLoadingDrawable = new LoadingDrawable(this);
+		mProgressBar.setIndeterminateDrawable(mLoadingDrawable);
+		mProgressBar.setVisibility(View.GONE);
+		mListView = (ListView) findViewById(android.R.id.list);
+		mListView.setVisibility(View.GONE);
+		mRetryTxt = (TextView) findViewById(R.id.retry);
+		mRetryTxt.setVisibility(View.GONE);
+		mRetryTxt.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO
+
+			}
+		});
 		mRes = getResources();
 		mSuggestLayout = (LinearLayout) findViewById(R.id.search_suggest_layout);
+		mSuggestLayoutAnimController = new LayoutAnimationController(
+				AnimationUtils.loadAnimation(this, R.anim.search_suggest_anim));
 		mSuggestWidget = new SuggestWidget();
 		mSuggestWidget.first = (TextView) findViewById(R.id.search_suggest_first);
 		mSuggestWidget.secondLeft = (TextView) findViewById(R.id.search_suggest_second_left);
@@ -57,9 +122,159 @@ public class SearchActivity extends Activity {
 		mSuggestWidget.sixthLeft = (TextView) findViewById(R.id.search_suggest_sixth_left);
 		mSuggestWidget.sixthRight = (TextView) findViewById(R.id.search_suggest_sixth_right);
 		mSuggestWidget.seventh = (TextView) findViewById(R.id.search_suggest_seventh);
+		mSuggestWidget.first.setOnClickListener(onClickListener);
+		mSuggestWidget.secondLeft.setOnClickListener(onClickListener);
+		mSuggestWidget.secondRight.setOnClickListener(onClickListener);
+		mSuggestWidget.thirdLeft.setOnClickListener(onClickListener);
+		mSuggestWidget.thirdRight.setOnClickListener(onClickListener);
+		mSuggestWidget.fourthLeft.setOnClickListener(onClickListener);
+		mSuggestWidget.fourthMiddle.setOnClickListener(onClickListener);
+		mSuggestWidget.fourthRight.setOnClickListener(onClickListener);
+		mSuggestWidget.fifthLeft.setOnClickListener(onClickListener);
+		mSuggestWidget.fifthRight.setOnClickListener(onClickListener);
+		mSuggestWidget.sixthLeft.setOnClickListener(onClickListener);
+		mSuggestWidget.sixthRight.setOnClickListener(onClickListener);
+		mSuggestWidget.seventh.setOnClickListener(onClickListener);
+		mGestureDetector = new GestureDetector(this,
+				new DefaultOnGestureListener());
 	}
 
-	private void test() {
+	private OnClickListener onClickListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// Log.d(TAG, "onClick");
+			String txt = null;
+			switch (v.getId()) {
+			case R.id.search_suggest_first:
+				txt = mSuggestWidget.first.getText().toString();
+				break;
+			case R.id.search_suggest_second_left:
+				txt = mSuggestWidget.secondLeft.getText().toString();
+				break;
+			case R.id.search_suggest_second_right:
+				txt = mSuggestWidget.secondRight.getText().toString();
+				break;
+			case R.id.search_suggest_third_left:
+				txt = mSuggestWidget.thirdLeft.getText().toString();
+				break;
+			case R.id.search_suggest_third_right:
+				txt = mSuggestWidget.thirdRight.getText().toString();
+				break;
+			case R.id.search_suggest_fourth_left:
+				txt = mSuggestWidget.fourthLeft.getText().toString();
+				break;
+			case R.id.search_suggest_fourth_middle:
+				txt = mSuggestWidget.fourthMiddle.getText().toString();
+				break;
+			case R.id.search_suggest_fourth_right:
+				txt = mSuggestWidget.fourthRight.getText().toString();
+				break;
+			case R.id.search_suggest_fifth_left:
+				txt = mSuggestWidget.fifthLeft.getText().toString();
+				break;
+			case R.id.search_suggest_fifth_right:
+				txt = mSuggestWidget.fifthRight.getText().toString();
+				break;
+			case R.id.search_suggest_sixth_left:
+				txt = mSuggestWidget.sixthLeft.getText().toString();
+				break;
+			case R.id.search_suggest_sixth_right:
+				txt = mSuggestWidget.sixthRight.getText().toString();
+				break;
+			case R.id.search_suggest_seventh:
+				txt = mSuggestWidget.seventh.getText().toString();
+				break;
+			}
+			if (txt != null) {
+				mKeysEdit.setText(txt);
+				onSearch(txt);
+			}
+		}
+	};
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (mSuggestLayout != null
+				&& mSuggestLayout.getVisibility() == View.VISIBLE) {
+			return mGestureDetector.onTouchEvent(event);
+		}
+		return super.onTouchEvent(event);
+	}
+
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent ev) {
+		// Log.d(TAG, "dispatchTouchEvent,ev : " + ev.toString());
+		if (mSuggestLayout != null
+				&& mSuggestLayout.getVisibility() == View.VISIBLE) {
+			if (mGestureDetector.onTouchEvent(ev)) {
+				return true;
+			}
+			if (ev.getAction() == MotionEvent.ACTION_UP
+					&& (xScrollDistance > MIN_DISTANCE || yScrollDistance > MIN_DISTANCE)) {
+				fillSuggest();
+			}
+		}
+		return super.dispatchTouchEvent(ev);
+	}
+
+	float startX;
+	float endX;
+	float startY;
+	float endY;
+	float xDelta;
+	float yDelta;
+	static final float MIN_DISTANCE = 50.0f;
+	private float xScrollDistance;
+	private float yScrollDistance;
+
+	private class DefaultOnGestureListener extends SimpleOnGestureListener {
+
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+			// Log.d(TAG, "onFling");
+			// startX = e1.getX();
+			// endX = e2.getX();
+			// startY = e1.getY();
+			// endY = e2.getY();
+			// xDelta = Math.abs(startX - endX);
+			// yDelta = Math.abs(startY - endY);
+			// if (xDelta > MIN_DISTANCE || yDelta > MIN_DISTANCE) {
+			// fillSuggest();
+			// return true;
+			// } else {
+			// return super.onFling(e1, e2, velocityX, velocityY);
+			// }
+			return super.onFling(e1, e2, velocityX, velocityY);
+		}
+
+		@Override
+		public boolean onScroll(MotionEvent e1, MotionEvent e2,
+				float distanceX, float distanceY) {
+			// Log.d(TAG, "onScroll,actiion : " + e2.getAction());
+			startX = e1.getX();
+			endX = e2.getX();
+			startY = e1.getY();
+			endY = e2.getY();
+			xDelta = Math.abs(startX - endX);
+			yDelta = Math.abs(startY - endY);
+			xScrollDistance = xDelta;
+			yScrollDistance = yDelta;
+			return super.onScroll(e1, e2, distanceX, distanceY);
+		}
+
+		@Override
+		public boolean onDown(MotionEvent e) {
+			// init
+			xScrollDistance = 0;
+			yScrollDistance = 0;
+			return super.onDown(e);
+		}
+
+	}
+
+	private void getDefaultKeys() {
 		InputStream is = null;
 		try {
 			is = mRes.openRawResource(R.raw.keys);
@@ -117,6 +332,8 @@ public class SearchActivity extends Activity {
 	private void fillSuggest() {
 		chooseKeys();
 		mSuggestLayout.setVisibility(View.VISIBLE);
+		mSuggestLayout.setLayoutAnimation(mSuggestLayoutAnimController);
+		mSuggestLayout.startLayoutAnimation();
 		mSuggestWidget.first.setText(mCurrentUseKeys[0]);
 		mSuggestWidget.secondLeft.setText(mCurrentUseKeys[1]);
 		mSuggestWidget.secondRight.setText(mCurrentUseKeys[2]);
@@ -130,6 +347,7 @@ public class SearchActivity extends Activity {
 		mSuggestWidget.sixthLeft.setText(mCurrentUseKeys[10]);
 		mSuggestWidget.sixthRight.setText(mCurrentUseKeys[11]);
 		mSuggestWidget.seventh.setText(mCurrentUseKeys[12]);
+
 	}
 
 	private void chooseKeys() {
@@ -165,5 +383,31 @@ public class SearchActivity extends Activity {
 		for (int i = 0; i < Constants.SUGGEST_KEYS_COUNT; i++) {
 			mCurrentUseKeys[i] = candidateKeys[positions[i]];
 		}
+	}
+
+	private void onSearch(String key) {
+		// TODO
+		mProgressBar.setVisibility(View.VISIBLE);
+		mSuggestLayout.setVisibility(View.GONE);
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK
+				&& mSuggestLayout.getVisibility() == View.GONE) {
+			// TODO cancel search operation,and show suggest page
+			if (mProgressBar.getVisibility() == View.VISIBLE) {
+				mProgressBar.setVisibility(View.GONE);
+			}
+			if (mListView.getVisibility() == View.VISIBLE) {
+				mListView.setVisibility(View.GONE);
+			}
+			if (mRetryTxt.getVisibility() == View.VISIBLE) {
+				mRetryTxt.setVisibility(View.GONE);
+			}
+			mSuggestLayout.setVisibility(View.VISIBLE);
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
 	}
 }
