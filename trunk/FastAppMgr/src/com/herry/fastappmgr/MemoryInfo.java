c0b696734c28be7b975962d02bba460ae0941fa4
Xinwei.Yi@gmail.com <Xinwei.Yi@gmail.com@021c3f65-3e9a-cb85-632d-0ac7946d1f4a>
2012-04-01 06:07:21 +0000

    应用管理：修改UI

--- trunk/FastAppMgr/src/com/herry/fastappmgr/MemoryInfo.java
+++ trunk/FastAppMgr/src/com/herry/fastappmgr/MemoryInfo.java
@@ -18,18 +18,28 @@ public final class MemoryInfo {
 	}
 
 	public String toString(Context ctx) {
+		String leftBranket = ctx.getString(R.string.left_bracket);
+		String totalTip = ctx.getString(R.string.total_tip);
+		String leftTip = ctx.getString(R.string.left_tip);
+		String rightBranket = ctx.getString(R.string.right_bracket);
 		StringBuilder sb = new StringBuilder();
-		sb.append(ctx.getString(R.string.ram_total_tip));
+		sb.append(ctx.getString(R.string.ram));
+		sb.append(leftBranket);
+		sb.append(totalTip);
 		sb.append(Utils.formatSize(mRamTotal));
-		sb.append("/");
-		sb.append(ctx.getString(R.string.ram_left_tip));
+		sb.append(ctx.getString(R.string.slash));
+		sb.append(leftTip);
 		sb.append(Utils.formatSize(mRamLeft));
+		sb.append(rightBranket);
 		sb.append("\n");
-		sb.append(ctx.getString(R.string.rom_total_tip));
+		sb.append(ctx.getString(R.string.rom));
+		sb.append(leftBranket);
+		sb.append(totalTip);
 		sb.append(Utils.formatSize(mRomTotal));
-		sb.append("/");
-		sb.append(ctx.getString(R.string.rom_left_tip));
+		sb.append(ctx.getString(R.string.slash));
+		sb.append(leftTip);
 		sb.append(Utils.formatSize(mRomLeft));
+		sb.append(rightBranket);
 		return sb.toString();
 	}
 }
