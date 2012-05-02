e2b5b2d9b81c5e75ff3dc7f8d0dc0b76411f8814
Xinwei.yI@gmail.com <Xinwei.yI@gmail.com@021c3f65-3e9a-cb85-632d-0ac7946d1f4a>
2012-05-02 14:58:31 +0000

    应用管理项目

--- FastAppMgr/src/com/herry/fastappmgr/view/WelcomeActivity.java
+++ FastAppMgr/src/com/herry/fastappmgr/view/WelcomeActivity.java
@@ -1,25 +1,43 @@
 package com.herry.fastappmgr.view;
 
-import com.herry.fastappmgr.R;
-import com.herry.fastappmgr.util.Utils;
+import java.util.List;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.herry.fastappmgr.R;
+import com.herry.fastappmgr.util.Utils;
+
 public class WelcomeActivity extends Activity {
 	private static final String TAG = "WelcomeActivity";
 
+	private Context mCtx;
+	private LayoutInflater mLayoutInflater;
 	private ProgressBar mProgressBar;
 	private TextView mContentTip;
 	private LinearLayout mContentLayout;
 
 	private String mBasicInfo;
+	private String mCpuInfo;
+	private String mScreenInfo;
+	private String mBatteryInfo;
+	
+	private List<PackageInfo> mAllData = null;
+	private List<Bundle> mDataList = null;
 	private Handler mHandler = new Handler() {
 		public void handleMessage(Message msg) {
 
@@ -35,7 +53,6 @@ public class WelcomeActivity extends Activity {
 
 			@Override
 			protected Void doInBackground(Void... params) {
-				// TODO
 				getBasicInfo();
 				mHandler.post(new Runnable() {
 
@@ -45,21 +62,42 @@ public class WelcomeActivity extends Activity {
 					}
 
 				});
+				getCpuInfo();
+				mHandler.post(new Runnable(){
+
+					@Override
+					public void run() {
+						mContentLayout.addView(genItemView(mCtx.getString(R.string.cpu_title),mCpuInfo));						
+					}
+					
+				});
+				getScreenInfo();
+				mHandler.post(new Runnable(){
+
+					@Override
+					public void run() {						
+						mContentLayout.addView(genItemView(mCtx.getString(R.string.screen_title),mScreenInfo));
+					}
+					
+				});
+				getBatteryInfo();
+				mHandler.post(new Runnable(){
+
+					@Override
+					public void run() {
+						mContentLayout.addView(genItemView(mCtx.getString(R.string.battery_title),mBatteryInfo));						
+					}
+					
+				});
 				return null;
 			}
-
-			protected void onProgressUpdate(Void[] values) {
-				
-			};
-
-			protected void onPostExecute(Void result) {
-				// TODO
-			};
 
 		}.execute();
 	}
 
 	private void initUI() {
+		mCtx = this;
+		mLayoutInflater = getLayoutInflater();
 		mProgressBar = (ProgressBar) findViewById(android.R.id.progress);
 		mContentTip = (TextView) findViewById(R.id.content_title);
 		mContentLayout = (LinearLayout) findViewById(R.id.content);
@@ -73,5 +111,25 @@ public class WelcomeActivity extends Activity {
 		mBasicInfo = Utils.getDevBasicInfo(this);
 	}
 	
+	private void getCpuInfo(){
+		mCpuInfo = Utils.getCpuInfo(this);
+	}
 	
+	private void getScreenInfo(){
+		mScreenInfo = Utils.getScreenInfo(this);
+	}
+	
+	private void getBatteryInfo(){
+		mBatteryInfo = Utils.getBatteryInfo(mCtx);
+	}
+	
+	private View genItemView(String tip,String content){
+		View v = mLayoutInflater.inflate(R.layout.welcome_item, null);
+		TextView title = (TextView)v.findViewById(R.id.title);
+		TextView info = (TextView)v.findViewById(R.id.info);
+		title.setText(tip);
+		info.setText(content);
+		return v;
+	}
+
 }
